ae84ac32f30120c3799a78ecff74d0b51c2a19f6
MD RUBEL <hello@mdrubel.net>
2019-07-04 17:07:47 +0600

    Works with all condition. All Done!

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -12,24 +12,61 @@ class ViewController: UIViewController {
     
     let backgroundImageView = UIImageView()
     var stories = Story()
+    var storyIndex: Int = 1
     
     @IBOutlet weak var storyTextView: UILabel!
     @IBOutlet weak var topButton: UIButton!
     @IBOutlet weak var bottomButton: UIButton!
+    @IBOutlet weak var restartButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setBackground()
-        
-        storyTextView.text = stories.Story1
-        topButton.setTitle(stories.​Answer1a, for: .normal)
-        bottomButton.setTitle(stories.​Answer1b, for: .normal)
+        restart()
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         
+        if sender.tag == 1 && (storyIndex == 1 || storyIndex == 2) {
+            storyTextView.text = stories.Story3
+            topButton.setTitle(stories.​Answer3a, for: .normal)
+            bottomButton.setTitle(stories.​Answer3b, for: .normal)
+            storyIndex = 3
+        }
+        else if sender.tag == 2 && storyIndex == 1 {
+            storyTextView.text = stories.Story2
+            topButton.setTitle(stories.​Answer2a, for: .normal)
+            bottomButton.setTitle(stories.​Answer2b, for: .normal)
+            storyIndex = 2
+        }
+        else if sender.tag == 2 && storyIndex == 2 {
+            storyTextView.text = stories.Story4
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            storyIndex = 4
+        }
+        else if sender.tag == 1 && storyIndex == 3 {
+            storyTextView.text = stories.Story6
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            storyIndex = 6
+        }
+        else if sender.tag == 2 && storyIndex == 3 {
+            storyTextView.text = stories.Story5
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            storyIndex = 5
+        }
         
+        if (storyIndex == 4 || storyIndex == 5 || storyIndex == 6) {
+            restartButton.isHidden = false
+        }
+        
+    }
+    
+    @IBAction func restartButtonPressed(_ sender: UIButton) {
+        restart()
     }
     
     //    MARK: Set Background Image
@@ -43,6 +80,16 @@ class ViewController: UIViewController {
         backgroundImageView.image = UIImage(named: "background")
         view.sendSubviewToBack(backgroundImageView)
         
+    }
+    
+    func restart() {
+        storyIndex = 1
+        restartButton.isHidden = true
+        storyTextView.text = stories.Story1
+        topButton.setTitle(stories.​Answer1a, for: .normal)
+        bottomButton.setTitle(stories.​Answer1b, for: .normal)
+        topButton.isHidden = false
+        bottomButton.isHidden = false
     }
 }
 
